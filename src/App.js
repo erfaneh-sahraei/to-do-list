5d910b97550851ed9d2613f1b6c977781bd81320
erfaneh <misssahraii@gmail.com>
2022-08-24 23:23:55 +0430

    some state added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCircleCheck , faPen , faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+// Tasks (ToDo List) State
+const [toDo, setToDo]= useState([
+  {"id":1, "title":"Task 1" , "status":false},
+  {"id":2, "title":"Task 2" , "status":false},
+]);
+
+// Temp State
+const [newTask , setNewTask] = useState('');
+const [updateData , setUpdateData] = useState('');
+
+//Add task
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br/><br/>
+      <h2>To Do List App(ReactJS)</h2>
+      <br/><br/>
     </div>
   );
 }
