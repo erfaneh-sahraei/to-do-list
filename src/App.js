9d069ee92244ff9fbd642b518212682809fbcab7
erfaneh <misssahraii@gmail.com>
2022-08-26 22:37:13 +0430

    Add update and delete button tasks added

--- src/App.js
+++ src/App.js
@@ -55,6 +55,36 @@ const updateTask=()=>{
       <br/><br/>
       <h2>لیست کارهای من</h2>
       <br/><br/>
+{/* Uppdate Task */}   
+
+<div className='row'>
+  <div className='col'>
+    <input className='form-control form-control-lg'/>
+  </div>
+  <div className='col-auto'>
+  <button className='btn btn-lg btn-success '>
+        آپدیت
+    </button>
+    <button className='btn btn-lg btn-warning '>
+        انصراف
+    </button>
+    </div>
+</div>
+<br/>
+
+{/* Add Task */}
+<div className='row'>
+  <div className='col'>
+    <input className='form-control form-control-lg'/>
+  </div>
+  <div className='col'>
+    <button className='btn btn-lg btn-success marginRight'>
+      اضافه کردن کار
+    </button>
+  </div>
+</div>
+<br/>
+
       {/*Display ToDos*/}
       {toDo && toDo.length ? '' : '!ایول کاری نداری'}
       {toDo && toDo
